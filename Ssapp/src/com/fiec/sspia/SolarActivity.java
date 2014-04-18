4e06a0d250d1465a0ab5a724adb84ff8082f10b8
iekumdario <jquinterov@ieee.org>
2014-04-18 00:47:46 -0500

    0.0.6 metodos y xml de satelites
    metodo para llenar tabla satelites createSatellites
    metodo para obtener satelites segun planeta - getSatellitesByPlanetId
    arreglo de nombres de satelites y de correspondencia satelite-planeta en
    xml

--- Ssapp/src/com/fiec/sspia/SolarActivity.java
+++ Ssapp/src/com/fiec/sspia/SolarActivity.java
@@ -15,100 +15,106 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.ViewConfiguration;
 
-public class SolarActivity extends Activity{
+public class SolarActivity extends Activity {
 	SolarDb db;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);		
+		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_solar);
-		setSettings();		
-		initialize();		
-		
+		setSettings();
+		initialize();
+
 		new SSClass(this);
-	}	
+	}
 
 	@Override
-    public boolean onCreateOptionsMenu(Menu menu) {
+	public boolean onCreateOptionsMenu(Menu menu) {
 		MenuInflater inflater = getMenuInflater();
 		inflater.inflate(R.menu.solar, menu);
-        return true;
-    }
+		return true;
+	}
 
-    @Override
-    public boolean onPrepareOptionsMenu(Menu menu) {
-        return super.onPrepareOptionsMenu(menu);
-    }
-    
-    @Override
-    protected void onPostCreate(Bundle savedInstanceState) {
-        super.onPostCreate(savedInstanceState);
-        SSClass.drawerToggle.syncState();
-    }
+	@Override
+	public boolean onPrepareOptionsMenu(Menu menu) {
+		return super.onPrepareOptionsMenu(menu);
+	}
 
-    @Override
-    public void onConfigurationChanged(Configuration newConfig) {
-        super.onConfigurationChanged(newConfig);
-        SSClass.drawerToggle.onConfigurationChanged(newConfig);
-    }
+	@Override
+	protected void onPostCreate(Bundle savedInstanceState) {
+		super.onPostCreate(savedInstanceState);
+		SSClass.drawerToggle.syncState();
+	}
 
+	@Override
+	public void onConfigurationChanged(Configuration newConfig) {
+		super.onConfigurationChanged(newConfig);
+		SSClass.drawerToggle.onConfigurationChanged(newConfig);
+	}
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        if (SSClass.drawerToggle.onOptionsItemSelected(item)) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-    
-    private boolean firstini(){
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		if (SSClass.drawerToggle.onOptionsItemSelected(item)) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
+	private boolean firstini() {
 		db.open();
 		String aux = db.getPlanet();
-		if(aux == null){
+		if (aux == null) {
 			db.close();
 			return true;
 		}
 		db.close();
-		return false;		
-    }
-    
-    private void initialize() {
+		return false;
+	}
+
+	private void initialize() {
 		db = new SolarDb(getApplicationContext());
-		
-		if(firstini() == true){
+
+		if (firstini() == true) {
 			String[] planets = this.getResources().getStringArray(R.array.theplanets);
 			int[] inits = this.getResources().getIntArray(R.array.planetscode);
-			Log.w("gmaTag", "inits="+inits.length);
+			Log.w("gmaTag", "inits=" + inits.length);
 			int[] inits2 = this.getResources().getIntArray(R.array.satcode);
-			Log.w("gmaTag", "inits2="+inits2.length);
+			Log.w("gmaTag", "inits2=" + inits2.length);
 			String[] dats;
 			db.open();
 			db.createPlanets(planets);
-			for(int i=0; i<inits.length; i++){
+			// para crear satelites
+			 String[] satellites = this.getResources().getStringArray(R.array.satellites);
+			 int[] satplanet= this.getResources().getIntArray(R.array.satelliteplanet);
+			 db.createSatellites(satellites,satplanet);
+
+			for (int i = 0; i < inits.length; i++) {
 				dats = this.getResources().getStringArray(inits[i]);
-				Log.w("gmaTag", "dats = " +dats[i]);
+				Log.w("gmaTag", "dats = " + dats[i]);
 				db.create(dats);
 			}
-			for(int i=0; i<inits2.length; i++){
+			for (int i = 0; i < inits2.length; i++) {
 				dats = this.getResources().getStringArray(inits2[i]);
 				db.create(dats);
 			}
-			
+
 			db.close();
-		}	
-		
+		}
+
 	}
 
 	public void setSettings() {
 		try {
-	        ViewConfiguration config = ViewConfiguration.get(this);
-	        Field menuKeyField = ViewConfiguration.class.getDeclaredField("sHasPermanentMenuKey");
-	        if(menuKeyField != null) {
-	            menuKeyField.setAccessible(true);
-	            menuKeyField.setBoolean(config, false);
-	        }
-	    } catch (Exception ex) {
-	        // Ignore
-	    }		
+			ViewConfiguration config = ViewConfiguration.get(this);
+			Field menuKeyField = ViewConfiguration.class
+					.getDeclaredField("sHasPermanentMenuKey");
+			if (menuKeyField != null) {
+				menuKeyField.setAccessible(true);
+				menuKeyField.setBoolean(config, false);
+			}
+		} catch (Exception ex) {
+			// Ignore
+		}
 	}
 
 }
--- Ssapp/src/com/fiec/sspia/db/SolarDb.java
+++ Ssapp/src/com/fiec/sspia/db/SolarDb.java
@@ -1,5 +1,7 @@
 package com.fiec.sspia.db;
 
+import java.util.ArrayList;
+
 import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
@@ -9,107 +11,140 @@ import android.util.Log;
 
 public class SolarDb {
 
-		private SQLiteDatabase db;
-		private DBHelper dbhelper;
-		
-		private String[] allColumns_planet = {DBHelper.IDPLANET, DBHelper.NAME,
-				DBHelper.IDDETALLE};
-		private String[] allColumns_detail = {DBHelper.TEMPMAX, 
-				 DBHelper.TEMP_MED,	DBHelper.TEMP_MIN, DBHelper.ICECOVER, DBHelper.SURFACE, DBHelper.MASS,
-				DBHelper.DIAMETER, DBHelper.MEAN_DEN, DBHelper.SCAP_VEL, DBHelper.AVDIS,
-				DBHelper.ROTPER, DBHelper.OBLIQUITI, DBHelper.ORBIT, DBHelper.ORBIT_ECC};
-		
-		public SolarDb(Context context){
-			dbhelper = new DBHelper(context);
+	private SQLiteDatabase db;
+	private DBHelper dbhelper;
+
+	private String[] allColumns_planet = { DBHelper.IDPLANET, DBHelper.NAME,
+			DBHelper.IDDETALLE };
+	private String[] allColumns_detail = { DBHelper.TEMPMAX, DBHelper.TEMP_MED,
+			DBHelper.TEMP_MIN, DBHelper.ICECOVER, DBHelper.SURFACE,
+			DBHelper.MASS, DBHelper.DIAMETER, DBHelper.MEAN_DEN,
+			DBHelper.SCAP_VEL, DBHelper.AVDIS, DBHelper.ROTPER,
+			DBHelper.OBLIQUITI, DBHelper.ORBIT, DBHelper.ORBIT_ECC };
+
+	public SolarDb(Context context) {
+		dbhelper = new DBHelper(context);
+	}
+
+	public void open() {
+		try {
+			db = dbhelper.getWritableDatabase();
+		} catch (SQLException ex) {
+			ex.printStackTrace();
 		}
-		
-		public void open(){
-			try{
-				db = dbhelper.getWritableDatabase();
+	}
+
+	public void close() {
+		dbhelper.close();
+	}
+
+	public boolean updatetemp(int id, String max, String med, String min) {
+		Log.w("gmaTag", "id:" + id + " max:" + max + " med:" + med + " min:"
+				+ min);
+		double medi = 0;
+		if (med.compareTo("null") == 0 || med.compareTo("0.0") == 0) {
+			medi = (Double.parseDouble(max) + Double.parseDouble(min)) / 2;
+			db.execSQL("update " + DBHelper.TABLEDETALLE + " set "
+					+ DBHelper.TEMPMAX + "='" + max + "', " + DBHelper.TEMP_MED
+					+ "='" + medi + "'," + DBHelper.TEMP_MIN + "='" + min
+					+ "' where " + DBHelper.IDDETALLE + "=" + id + ";");
+		}
+		return true;
+	}
+
+	public String getPlanet() {
+		Cursor cursor = db.query(DBHelper.TABLEPLANET, allColumns_planet, null,
+				null, null, null, null);
+		if (cursor.getCount() == 0) {
+			return null;
+		}
+		cursor.move(1);
+		return cursor.getString(1);
+	}
+
+	public int getIdbyPname(String planet) {
+		int id;
+		Cursor cursor = db.query(DBHelper.TABLEPLANET, allColumns_planet, null,
+				null, null, null, null);
+		cursor.moveToFirst();
+		while (!cursor.isAfterLast()) {
+			if (cursor.getString(1).compareTo(planet) == 0) {
+				id = cursor.getInt(0);
+				cursor.close();
+				return id;
 			}
-			catch(SQLException ex){
-				ex.printStackTrace();
-			}
+			cursor.moveToNext();
 		}
-		
-		public void close(){
-			dbhelper.close();
+		cursor.close();
+		return 0;
+	}
+
+	public String[] getDetails(int id) {
+		int i = 1, j = 0;
+		String[] aux = new String[14];
+		Cursor cursor = db.query(DBHelper.TABLEDETALLE, allColumns_detail,
+				null, null, null, null, null);
+		// Cursor cursor = db.query(DBHelper.TABLEDETALLE, allColumns_detail,
+		// DBHelper.IDDETALLE+"="+id, null, null, null,null);
+		cursor.move(id);
+		for (int k = 0; k < 14; k++) {
+			aux[k] = cursor.getString(k);
+			Log.w("gmaTag", aux[k]);
+			// Log.w("gmaTag", "aux = "+aux[k]);
 		}
-		
-		public boolean updatetemp(int id, String max, String med, String min){
-			Log.w("gmaTag", "id:"+id+" max:"+max+" med:"+med+" min:"+min);
-			double medi = 0;
-			if(med.compareTo("null")==0 || med.compareTo("0.0")==0){
-				medi = (Double.parseDouble(max)+Double.parseDouble(min))/2;
-				db.execSQL("update "+DBHelper.TABLEDETALLE+" set "+DBHelper.TEMPMAX+"='"+
-						max+"', "+DBHelper.TEMP_MED+"='"+medi+"',"+DBHelper.TEMP_MIN+"='"+min+"' where "+DBHelper.IDDETALLE+"="+id+";");
-			}	
-			return true;
+		/*
+		 * while(!cursor.isAfterLast()){ aux[j] = cursor.getString(i);
+		 * Log.w("gmaTag", "aux = "+aux[j]); j++; i++; cursor.moveToNext(); }
+		 */
+		cursor.close();
+		return aux;
+	}
+
+	public boolean createPlanets(String[] dats) {
+		for (int i = 0; i < dats.length; i++)
+			db.execSQL("insert into " + DBHelper.TABLEPLANET + "("
+					+ DBHelper.NAME + "," + DBHelper.IDDETALLE + ") values ('"
+					+ dats[i] + "'," + (i + 1) + ")");
+		return true;
+	}
+
+	public boolean create(String[] dats) {
+		db.execSQL("INSERT INTO " + DBHelper.TABLEDETALLE + "("
+				+ DBHelper.TEMPMAX + "," + DBHelper.TEMP_MED + ","
+				+ DBHelper.TEMP_MIN + "," + DBHelper.ICECOVER + ","
+				+ DBHelper.SURFACE + "," + DBHelper.MASS + ","
+				+ DBHelper.DIAMETER + "," + DBHelper.MEAN_DEN + ","
+				+ DBHelper.SCAP_VEL + "," + DBHelper.AVDIS + ","
+				+ DBHelper.ROTPER + "," + DBHelper.OBLIQUITI + ","
+				+ DBHelper.ORBIT + "," + DBHelper.ORBIT_ECC + ")"
+				+ " VALUES ('" + dats[0] + "','" + dats[1] + "','" + dats[2]
+				+ "','" + dats[3] + "','" + dats[4] + "','" + dats[5] + "','"
+				+ dats[6] + "','" + dats[7] + "','" + dats[8] + "','" + dats[9]
+				+ "','" + dats[10] + "','" + dats[11] + "','" + dats[12]
+				+ "','" + dats[13] + "')");
+		return true;
+	}
+
+	// agrega datos a tabla "table_moon"
+	public boolean createSatellites(String[] satName, int[] satPlanet) {
+		for (int i = 0; i < satPlanet.length; i++) {
+			db.execSQL("INSERT INTO " + DBHelper.TABLEMOON + "("
+					+ DBHelper.NAME + "," + DBHelper.IDDETALLE + ","
+					+ DBHelper.IDPLANET + ")" + " VALUES ('" + satName[i]
+					+ "','" + (i + 8) + "','" + satPlanet[i] + "')");
 		}
-		
-		public String getPlanet(){
-			Cursor cursor = db.query(DBHelper.TABLEPLANET, allColumns_planet, null, null, null, null, null);
-			if(cursor.getCount()==0){			
-				return null;
-			}
-			cursor.move(1);
-			return cursor.getString(1);		
-		}
-		
-		public int getIdbyPname(String planet){		
-			int id;
-			Cursor cursor = db.query(DBHelper.TABLEPLANET, allColumns_planet,
-					null, null, null, null, null);
-			cursor.moveToFirst();
-			while(!cursor.isAfterLast()){
-				if(cursor.getString(1).compareTo(planet)==0){
-					id = cursor.getInt(0);
-					cursor.close();
-					return id;
-				}				
-				cursor.moveToNext();		
-			}
-			cursor.close();
-			return 0;
-		}
-		
-		public String[] getDetails(int id){
-			int i = 1, j = 0;
-			String[] aux = new String[14];
-			Cursor cursor = db.query(DBHelper.TABLEDETALLE, allColumns_detail, null, null, null, null,null);
-			//Cursor cursor = db.query(DBHelper.TABLEDETALLE, allColumns_detail, DBHelper.IDDETALLE+"="+id, null, null, null,null);
-			cursor.move(id);
-			for(int k=0; k<14; k++){
-				aux[k] = cursor.getString(k);
-				Log.w("gmaTag", aux[k]);
-				//Log.w("gmaTag", "aux = "+aux[k]);
-			}
-			/*while(!cursor.isAfterLast()){
-				aux[j] = cursor.getString(i);
-				Log.w("gmaTag", "aux = "+aux[j]);
-				j++; i++;
-				cursor.moveToNext();
-			}*/
-			cursor.close();
-			return aux;
-		}
-		
-		public boolean createPlanets(String[] dats){
-			for(int i=0;i<dats.length;i++)
-			db.execSQL("insert into "+DBHelper.TABLEPLANET+"("+DBHelper.NAME+","+DBHelper.IDDETALLE+") values ('"+dats[i]+"',"+(i+1)+")");
-			return true;
-		}
-		
-		public boolean create(String[] dats){		
-			db.execSQL("INSERT INTO "
-			+DBHelper.TABLEDETALLE+ "("+DBHelper.TEMPMAX+","+DBHelper.TEMP_MED+","+
-			DBHelper.TEMP_MIN+","+DBHelper.ICECOVER+","+DBHelper.SURFACE+","+DBHelper.MASS+
-			","+DBHelper.DIAMETER+","+DBHelper.MEAN_DEN+","+DBHelper.SCAP_VEL+","+DBHelper.AVDIS+
-			","+DBHelper.ROTPER+","+DBHelper.OBLIQUITI+","+DBHelper.ORBIT+","+DBHelper.ORBIT_ECC+")"+
-			" VALUES ('"+dats[0]+"','"+dats[1]+"','"+dats[2]+"','"+dats[3]+"','"+dats[4]+
-			"','"+dats[5]+"','"+dats[6]+"','"+dats[7]+"','"+dats[8]+"','"+dats[9]+"','"+dats[10]+
-			"','"+dats[11]+"','"+dats[12]+"','"+dats[13]+"')");
-			return true;
-		}
-		
+		return true;
+	}
+
+	// obtiene id de lunas de planeta segun id de planeta, compatible con getdetails
+	public int[] getSatellitesByPlanetId(int PlanetId) {
+		Cursor cursor = db.query(DBHelper.TABLEMOON,
+				new String[] { DBHelper.IDLUNA }, DBHelper.IDPLANET + "="
+						+ PlanetId, null, null, null, null);
+		int[] satellites = new int[cursor.getCount()];
+		for (int i = 0; i < cursor.getCount(); i++)
+			satellites[i] = cursor.getInt(0);
+		cursor.close();
+		return satellites;
+	}
 }
